194cc4a85ae9365900f476b99708e02f276f38ea
David Vega <david.vega@crowdint.com>
2013-01-20 14:20:23 -0600

    first level passed

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,5 +1,10 @@
 require File.dirname(__FILE__) + '/../rubywarrior/test-drive-beginner/player'
 
 describe "The Ruby Warrior's life" do
- 
+  let(:warrior){ mock}
+  it 'should walk forward' do
+    warrior.should_receive(:walk!)
+    player = Player.new
+    player.play_turn(warrior)
+  end 
 end
