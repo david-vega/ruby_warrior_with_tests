620b8a7e9e9a380d8d6d4f1a989811837ec33aaa
David Vega <david.vega@crowdint.com>
2013-01-20 21:01:00 -0600

    Second level passed

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -2,9 +2,19 @@ require File.dirname(__FILE__) + '/../rubywarrior/test-drive-beginner/player'
 
 describe "The Ruby Warrior's life" do
   let(:warrior){ mock}
-  it 'should walk forward' do
+  before do
+    @player = Player.new
+  end
+
+  it 'should walk forward when there is no enemies' do
+    warrior.stub_chain(:feel, :empty?).and_return true
     warrior.should_receive(:walk!)
-    player = Player.new
-    player.play_turn(warrior)
+    @player.play_turn(warrior)
   end 
+
+  it 'should attack when there is enemies' do
+    warrior.stub_chain(:feel, :empty?).and_return false
+    warrior.should_receive(:attack!)
+    @player.play_turn(warrior)
+  end
 end
